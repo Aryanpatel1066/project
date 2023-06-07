c0c25fcd9aafa4b122c1bff319bca16865bb87b9
Aryan_patel <112760422+Aryanpatel1066@users.noreply.github.com>
2023-06-07 17:45:53 +0530

    Initial commit

---
+++ src/Components/Header/index.js
@@ -0,0 +1,176 @@
+import { HomeFilled, ShoppingCartOutlined } from "@ant-design/icons";
+import {
+  Badge,
+  Button,
+  Checkbox,
+  Drawer,
+  Form,
+  Input,
+  InputNumber,
+  Menu,
+  message,
+  Table,
+  Typography
+} from "antd";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { getCart } from "../../API";
+
+function AppHeader() {
+  const navigate = useNavigate();
+
+  const onMenuClick = (item) => {
+    navigate(`/${item.key}`);
+  };
+  return (
+    <div className="appHeader">
+      <Menu
+        className="appMenu"
+        onClick={onMenuClick}
+        mode="horizontal"
+        items={[
+          {
+            label: <HomeFilled />,
+            key: ""
+          },
+          {
+            label: "Men",
+            key: "men",
+            children: [
+              {
+                label: "Men's Shirts",
+                key: "mens-shirts"
+              },
+              {
+                label: "Men's Shoes",
+                key: "mens-shoes"
+              },
+              {
+                label: "Men's Watches",
+                key: "mens-watches"
+              }
+            ]
+          },
+          {
+            label: "Women",
+            key: "women",
+            children: [
+              {
+                label: "Women's Dresses",
+                key: "womens-dresses"
+              },
+              {
+                label: "Women's Shoes",
+                key: "womens-shoes"
+              },
+              {
+                label: "Women's Watches",
+                key: "womens-watches"
+              },
+              {
+                label: "Women's Bags",
+                key: "womens-bags"
+              },
+              {
+                label: "Women's Jewellery",
+                key: "womens-jewellery"
+              }
+            ]
+          },
+          {
+            label: "Fragrances",
+            key: "fragrances"
+          }
+        ]}
+      />
+      <Typography.Title>My Store</Typography.Title>
+      <AppCart />
+    </div>
+  );
+}
+function AppCart() {
+  const [cartDrawerOpen, setCartDrawerOpen] = useState(false);
+  // const [checkoutDrawerOpen, setCheckoutDrawerOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  useEffect(() => {
+    getCart().then((res) => {
+      setCartItems(res.products);
+    });
+  }, []);
+
+  return (
+    <div>
+      <Badge
+        onClick={() => {
+          setCartDrawerOpen(true);
+        }}
+        count={cartItems.length}
+        className="soppingCartIcon"
+      >
+        <ShoppingCartOutlined />
+      </Badge>
+      <Drawer
+        open={cartDrawerOpen}
+        onClose={() => {
+          setCartDrawerOpen(false);
+        }}
+        title="Your Cart"
+        contentWrapperStyle={{ width: 500 }}
+      >
+        <Table
+          pagination={false}
+          columns={[
+            {
+              title: "Title",
+              dataIndex: "title"
+            },
+            {
+              title: "Price",
+              dataIndex: "price",
+              render: (value) => {
+                return <span>${value}</span>;
+              }
+            },
+            {
+              title: "Quantity",
+              dataIndex: "quantity",
+              render: (value, record) => {
+                return (
+                  <InputNumber
+                    min={0}
+                    defaultValue={value}
+                    onChange={(value) => {
+                      setCartItems((pre) =>
+                        pre.map((cart) => {
+                          if (record.id === cart.id) {
+                            cart.total = cart.price * value;
+                          }
+                          return cart;
+                        })
+                      );
+                    }}
+                  ></InputNumber>
+                );
+              }
+            },
+            {
+              title: "Total",
+              dataIndex: "total",
+              render: (value) => {
+                return <span>${value}</span>;
+              }
+            }
+          ]}
+          dataSource={cartItems}
+          summary={(data) => {
+            const total = data.reduce((pre, current) => {
+              return pre + current.total;
+            }, 0);
+            return <span>Total: ${total}</span>;
+          }}
+        />
+      </Drawer>
+    </div>
+  );
+}
+export default AppHeader;
